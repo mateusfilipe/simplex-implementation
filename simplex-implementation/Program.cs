5fb2c26ac68d56e4a86cf04102354c780a7ff80f
RodrigoMalaquias <48831753+RodrigoMalaquias@users.noreply.github.com>
2021-04-28 18:38:44 -0300

    Iniciando vetore

--- simplex-implementation/Program.cs
+++ simplex-implementation/Program.cs
@@ -6,7 +6,10 @@ namespace simplex_implementation
     {
         static void Main(string[] args)
         {
-            // Que comecem os jogos !!
+            int[] c = new int[5] { 3, 5, 0, 0, 0 };
+            int[,] a = new int[,] { { 2, 4, 1, 0, 0 }, { 6,1,0,1,0 }, { 1,-1,0,0,1} };
+            /*int[,] array2D = new int[,] { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } };*/
+            int[,] b = new int[,] { {10}, { 20 }, { 30} };
         }
     }
 }

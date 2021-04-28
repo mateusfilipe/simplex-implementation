dc5949a55930b9df10e0148d2b80d79acff7a019
RodrigoMalaquias <48831753+RodrigoMalaquias@users.noreply.github.com>
2021-04-28 19:40:41 -0300

    Testando Lista e Vetores

--- simplex-implementation/Program.cs
+++ simplex-implementation/Program.cs
@@ -7,27 +7,37 @@ namespace simplex_implementation
     {
         static void Main(string[] args)
         {
-            int[] c = new int[5] { 3, 5, 0, 0, 0 };
+            int[] c = new int[] { 3, 5, 0, 0, 0 };
             int[] b = new int[] {10, 20, 30};
             int[,] a = new int[,] { { 2, 4, 1, 0, 0 }, { 6, 1, 0, 1, 0 }, { 1, -1, 0, 0, 1 } };
 
+
+
             List<float> C_funcaoobjetiva = new List<float>();
             List<float> A_restricoes = new List<float>();
-            List<float> B_igualdades = new List<float>();
+            List<float> B_restricoes = new List<float>();
 
             for(int i = 0; i < c.Length; i++)
             {
                 C_funcaoobjetiva.Add(c[i]);
             }
+            C_funcaoobjetiva.Add(0);
+
             for (int i = 0; i < b.Length; i++)
             {
-                B_igualdades.Add(b[i]);
+                float[] aux = new float[] { };
+                for (int j = 0; j < c.Length; j++)
+                {
+                    A_restricoes.Add(a[i, j]);
+                }
+                
             }
 
-            foreach (float dadoB in B_igualdades)
+            foreach (float dadoB in B_restricoes)
             {
                 Console.WriteLine(dadoB);
             }
+
 
         }
     }

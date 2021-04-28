17067092eed4e1520ab2b0cac069ea78eac52184
RodrigoMalaquias <48831753+RodrigoMalaquias@users.noreply.github.com>
2021-04-28 20:12:03 -0300

    Implementando Restrição a completa

--- simplex-implementation/Program.cs
+++ simplex-implementation/Program.cs
@@ -7,9 +7,9 @@ namespace simplex_implementation
     {
         static void Main(string[] args)
         {
-            int[] c = new int[] { 3, 5, 0, 0, 0 };
-            int[] b = new int[] {10, 20, 30};
-            int[,] a = new int[,] { { 2, 4, 1, 0, 0 }, { 6, 1, 0, 1, 0 }, { 1, -1, 0, 0, 1 } };
+            float[] c = new float[] { 3, 5, 0, 0, 0 };
+            float[] b = new float[] {10, 20, 30};
+            float[,] a = new float[,] { { 2, 4, 1, 0, 0 }, { 6, 1, 0, 1, 0 }, { 1, -1, 0, 0, 1 } };
 
 
 
@@ -17,27 +17,29 @@ namespace simplex_implementation
             List<float> A_restricoes = new List<float>();
             List<float> B_restricoes = new List<float>();
 
-            for(int i = 0; i < c.Length; i++)
-            {
-                C_funcaoobjetiva.Add(c[i]);
-            }
+            List<float> A_restricoesFull = new List<float>();
+
+
+
+            C_funcaoobjetiva.AddRange(c);
             C_funcaoobjetiva.Add(0);
 
             for (int i = 0; i < b.Length; i++)
             {
-                float[] aux = new float[] { };
                 for (int j = 0; j < c.Length; j++)
                 {
-                    A_restricoes.Add(a[i, j]);
+                    A_restricoesFull.Add(a[i, j]);
                 }
-                
+                A_restricoesFull.Add(b[i]);
             }
 
-            foreach (float dadoB in B_restricoes)
+
+            
+            foreach (float dadoAfull in A_restricoesFull)
             {
-                Console.WriteLine(dadoB);
+                  Console.Write(dadoAfull);  
             }
-
+            
 
         }
     }

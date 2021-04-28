090c13f17ba5eaf9e06b6ae7b947f55016b70340
RodrigoMalaquias <48831753+RodrigoMalaquias@users.noreply.github.com>
2021-04-28 18:56:41 -0300

    Upando

--- simplex-implementation/Program.cs
+++ simplex-implementation/Program.cs
@@ -8,8 +8,18 @@ namespace simplex_implementation
         {
             int[] c = new int[5] { 3, 5, 0, 0, 0 };
             int[,] a = new int[,] { { 2, 4, 1, 0, 0 }, { 6,1,0,1,0 }, { 1,-1,0,0,1} };
-            /*int[,] array2D = new int[,] { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } };*/
             int[,] b = new int[,] { {10}, { 20 }, { 30} };
+
+            int[,] restricoes = new int[,] { { 2, 4, 1, 0, 0 }, { 6, 1, 0, 1, 0 }, { 1, -1, 0, 0, 1 } };
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write(a[i,j]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }

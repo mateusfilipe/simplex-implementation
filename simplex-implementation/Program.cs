5cd432ae0dfb64c6f7677de69d0ebe88b1c77f2f
Mateus Filipe <mateusfilipe557@gmail.com>
2021-04-28 19:09:23 -0300

    Adicionando lógica de lista ao código.

--- simplex-implementation/Program.cs
+++ simplex-implementation/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace simplex_implementation
 {
@@ -7,19 +8,27 @@ namespace simplex_implementation
         static void Main(string[] args)
         {
             int[] c = new int[5] { 3, 5, 0, 0, 0 };
-            int[,] a = new int[,] { { 2, 4, 1, 0, 0 }, { 6,1,0,1,0 }, { 1,-1,0,0,1} };
-            int[,] b = new int[,] { {10}, { 20 }, { 30} };
+            int[] b = new int[] {10, 20, 30};
+            int[,] a = new int[,] { { 2, 4, 1, 0, 0 }, { 6, 1, 0, 1, 0 }, { 1, -1, 0, 0, 1 } };
 
-            int[,] restricoes = new int[,] { { 2, 4, 1, 0, 0 }, { 6, 1, 0, 1, 0 }, { 1, -1, 0, 0, 1 } };
+            List<float> C_funcaoobjetiva = new List<float>();
+            List<float> A_restricoes = new List<float>();
+            List<float> B_igualdades = new List<float>();
 
-            for (int i = 0; i < 5; i++)
+            for(int i = 0; i < c.Length; i++)
             {
-                for (int j = 0; j < 5; j++)
-                {
-                    Console.Write(a[i,j]);
-                }
-                Console.WriteLine();
+                C_funcaoobjetiva.Add(c[i]);
             }
+            for (int i = 0; i < b.Length; i++)
+            {
+                B_igualdades.Add(b[i]);
+            }
+
+            foreach (float dadoB in B_igualdades)
+            {
+                Console.WriteLine(dadoB);
+            }
+
         }
     }
 }

13cfca333438930ddca344f6f423a99e2e7dfee0
Mateus Filipe <mateusfilipe557@gmail.com>
2021-04-28 18:20:57 -0300

    Inicio do projeto

---
+++ simplex-implementation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace simplex_implementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
